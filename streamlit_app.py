c0bf00278b203b27ed87fa94b468b226df7c6023
Ricardo Correia <ricardocorreiaengenhariafisica@gmail.com>
2025-02-14 11:13:57 +0000

    Changing API response

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ ingredients_list = st.multiselect(
 )
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
 
 if ingredients_list:
